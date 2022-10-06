5409af89bbc19be8f07de73353ffd4151a765f9a
Dmitriy <kapralov_00@mail.ru>
2022-10-06 19:37:05 +0300

    Убраны длинные строки и лишние пробелы

--- src/CalcLabRating.py
+++ src/CalcLabRating.py
@@ -15,11 +15,8 @@ class CalcLabRating:
         self.rating = CalcRating(self.data).calc()
         n = math.ceil(len(self.rating) / 4)
 
-        self.rating = dict(sorted(self.rating.items(), reverse=True, key=lambda x: x[1])[:n])
-
+        self.rating = dict(sorted
+                           (self.rating.items(),
+                            reverse=True,
+                            key=lambda x: x[1])[:n])
         return self.rating
-
-
-
-
-
