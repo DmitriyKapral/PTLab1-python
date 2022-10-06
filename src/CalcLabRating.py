9028a4ebd2137e9964244725417145590c6a2de7
Dmitriy <kapralov_00@mail.ru>
2022-10-06 18:59:51 +0300

    Добавлен класс, который возвращает первый квантиль студентов

---
+++ src/CalcLabRating.py
@@ -0,0 +1,25 @@
+from Types import DataType
+from CalcRating import CalcRating
+import math
+
+RatingType = dict[str, float]
+
+
+class CalcLabRating:
+
+    def __init__(self, data: DataType) -> None:
+        self.data: RatingType = data
+        self.rating: RatingType = {}
+
+    def calc(self) -> RatingType:
+        self.rating = CalcRating(self.data).calc()
+        n = math.ceil(len(self.rating) / 4)
+
+        self.rating = dict(sorted(self.rating.items(), reverse=True, key=lambda x: x[1])[:n])
+
+        return self.rating
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import sys
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
 from LabDataReader import LabDataReader
+from CalcLabRating import CalcLabRating
 
 
 def get_path_from_arguments(args) -> str:
@@ -22,7 +23,7 @@ def main():
     students = reader.read(path)
     print("Students: ", students)
 
-    rating = CalcRating(students).calc()
+    rating = CalcLabRating(students).calc()
     print("Rating: ", rating)
 
 
